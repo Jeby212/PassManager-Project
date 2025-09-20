342d4c8743de386bab6448badde763c6333eb826
Karim Singer <karim.singer2003@gmail.com>
2025-09-20 14:46:25 +0200

    added password retrieval and decryption

--- pass_manager.py
+++ pass_manager.py
@@ -6,7 +6,7 @@ pw = ''
 import base64
 from itertools import cycle
 
-MASTER = input("Enter a master password (remember it):\n")
+MASTER = input("Enter a master password (remember it):\n")  #Be sure to use the same master every time or else decryption won't work due to different key
 
 def _xor_bytes(data: bytes, key: bytes) -> bytes:
     return bytes(d ^ k for d, k in zip(data, cycle(key)))
@@ -35,7 +35,20 @@ def addPassword():
     file = open(filepath, 'a')
     file.writelines(lines)
 
+def decryptPassword():
+    file = open(filepath, 'r')
+    lines = file.readlines()
+    word = encrypt(input("Which service\'s username and password do you wish to retrieve? "), MASTER)  # String to search for
+    for row in lines:
+        if row.find(word) != -1:
+            num = lines.index(row)
+            print("Service: " + try_decrypt(lines[num], MASTER))
+            print("Username: " + try_decrypt(lines[num + 1], MASTER))
+            print("Password: " + try_decrypt(lines[num + 2], MASTER))
+    file.close()
+
 addPassword()
+decryptPassword()
 
 with open(filepath, 'r') as fr:
     info = [try_decrypt(x, MASTER) for x in fr.read().splitlines()]
